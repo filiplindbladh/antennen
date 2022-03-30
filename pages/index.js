c77e5e8c21f07477dbb54d487f63fa96407839d9
Filip Lindbladh <>
2022-03-30 19:54:48 +0200

    feat: use scss files

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import { apiKey, wpBaseUri, mixlrApi } from "../apiKey";
 import axios from "axios";
 import Head from "next/head";
